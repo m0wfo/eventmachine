870553fcbf719757b11fc976e9b3cd20cf97f08a
Chris Mowforth <chris@mowforth.com>
2012-09-10 14:32:49 +0100

    more porting...

--- lib/em_reactor.rb
+++ lib/em_reactor.rb
@@ -36,12 +36,12 @@ class EmReactor
   def run
     begin
       @my_selector = Selector.open()
-      @b_run_reactor = true
+      @run_reactor = true
     rescue (IOException e)
       p 'err!'
     end
 
-    while @b_run_reactor
+    while @run_reactor
       run_loop_breaks
       break if !@b_run_reactor
 
@@ -58,19 +58,14 @@ class EmReactor
   end
 
   def add_new_connections
-    iter = @unbound_connections.listIterator(0)
-    while iter.hasNext()
-      ec = iter.next()
+    @unbound_connections.each do |ec|
       ec.cleanup()
     end
     @detached_connections.clear()
 
-    iter2 = @new_connections.listIterator(0)
-    while iter2.hasNext()
-      b = iter2.next()
-
+    @new_connections.each do |b|
       ec = @connections.get(b)
-      if !ec
+      if ec.nil?
         begin
           ec.register()
         rescue ClosedChannelException e
@@ -82,9 +77,7 @@ class EmReactor
   end
 
   def remove_unbound_connections
-    iter = @unbound_connections.listIterator(0)
-    while iter.hasNext()
-      b = iter.next()
+    @unbound_connections.each do |b|
       ec = @connections.remove(b)
 
       if !ec
@@ -131,7 +124,7 @@ class EmReactor
 
   def process_io
     it = @my_selector.selectedKeys().iterator()
-    while it.hasNext()
+    while it.hasNext() do
       k = it.next()
       it.remove()
 
@@ -218,67 +211,191 @@ class EmReactor
   def is_connectable(key)
     ec = key.attachment()
     b = ec.getBinding()
+
+    begin
+      if ec.finishConnecting()
+        event_callback(b, EM_CONNECTION_COMPLETED, nil)
+      else
+        @unbound_connections.add(b)
+      end
+    rescue IOException e
+      @unbound_connections.add(b)
+    end
   end
 
   def close
+    @my_selector.close() if !@my_selector.nil?
+
+    @acceptors.each {|a| a.close() }
+
+    conns = ArrayList.new
+
+    @connections.each do |ec|
+      conns.add(ec) if !ec.nil?
+    end
+    @connections.clear()
+
+    @conns.each do |ec|
+      event_callback(ec.get_binding, EM_CONNECTION_UNBOUND, nil)
+      ec.close
+      @detached_connections.add(ec) if sc.is_attached
+    end
+
+    @detached_connections.each { |ec| ec.cleanup }
+    @detached_connections.clear()
   end
 
   def run_loop_breaks
+    event_callback(0, EM_LOOPBREAK_SIGNAL, nil) if @loop_breaker.getAndSet(false)
   end
 
   def stop
+    @run_reactor = false
+    signal_loop_break    
   end
 
   def run_timers
+    now = java.util.Date.new.getTime
+    while !@timers.isEmpty
+      k = @timers.firstKey
+      break if k > now
+
+      callbacks = @timers.get(k)
+      @timers.remove(k)
+
+      callbacks.each { |cb| event_callback(0, EM_TIMER_FIRED, nil, cb.longValue) }
+    end
   end
 
-  def install_oneshot_timer
+  def install_oneshot_timer(milliseconds)
+    s = create_binding
+    deadline = java.util.Date.new.getTime + milliseconds
+
+    if @timers.containsKey(deadline)
+      @timers.get(deadline).add(s)
+    else
+      callbacks = ArrayList.new
+      callbacks.add(s)
+      @timers.put(deadline, callbacks)
+    end
   end
 
-  def start_tcp_server
+  def start_tcp_server(addr, port=nil)
+    if port.nil?
+      addr = InetSocketAddress.new(addr, port)
+    end
+    
+    begin
+      server = ServerSocketChannel.open
+      server.configureBlocking(false)
+      server.socket.bind(addr)
+      s = create_binding
+      @acceptors.put(s, server)
+      server.register(@my_selector, SelectionKey::OP_ACCEPT, s)
+      return s
+    rescue IOException e
+      raise err
+    end
   end
 
-  def stop_tcp_server
+  def stop_tcp_server(sig)
+    server = @acceptors.remove(sig)
+    if !server.nil?
+      server.close
+    else
+      raise "err"
+    end
   end
 
-  def open_udp_socket
+  def open_udp_socket(addr, port=nil)
+    if port.nil?
+      addr = InetSocketAddress.new(addr, port)
+    end
+    
+    dg = DatagramChannel.open
+    dg.configureBlocking(false)
+    dg.socket.bind(addr)
+
+    b = create_binding
+    ec = EventableDatagramChannel.new(dg, b, @my_selector)
+    dg.register(@my_selector, SelectionKey::OP_READ, ec)
+    @connections.put(b, ec)
+    return b
   end
 
-  def send_data
+  def send_data(sig, data)
+    @connections.get(sig).schedule_outbound_data(ByteBuffer.wrap(data))
   end
 
   def set_comm_inactivity_timeout(sig, mills)
     @connections.get(sig).setCommInactivityTimeout(mills)
   end
 
-  def send_datagram
+  def send_datagram(sig, data, addr, port=nil)
+    @connections.get(sig).schedule_outbound_datagram(ByteBuffer.wrap(data), addr, port)
   end
 
-  def connect_tcp_server
+  def connect_tcp_server(addr, port, bind_addr=nil, bind_port=nil)
+    b = create_binding
+
+    begin
+      sc = SocketChannel.open
+      sc.configureBlocking(false)
+
+      sc.bind(InetSocketAddress.new(bind_addr, bind_port)) if !bind_addr.nil?
+
+      ec = EventableSocketChannel.new(sc, b, @my_selector)
+
+      if sc.connect(InetSocketAddress.new(addr, port))
+        raise "immediate-connect unimplemented"
+      else
+        ec.set_connect_pending
+        @connections.put(b, ec)
+        @new_connections.add(b)
+      end
+    rescue IOException e
+      p "immediate-connect unimplemented"
+    end
+
+    return b
   end
 
-  def close_connection
+  def close_connection(sig, after_writing)
+    ec = @connections.get(sig)
+    if !ec.nil?
+      ec.schedule_close(after_writing)
+      @unbound_connections.add(sig)
+    end
   end
 
   def create_binding
+    return @binding_index += 1
   end
 
   def signal_loop_break
+    @loop_breaker.set(true)
+    @my_selector.wakeup if !@my_selector.nil?
   end
 
-  def start_tls
+  def start_tls(sig)
+    @connections.get(sig).start_tls
   end
 
-  def set_timer_quantum
+  def timer_quantum=(ms)
+    raise "err" if (ms < 5 || ms > 2500)
+    @timer_quantum = ms
   end
 
   def get_peer_name(sig)
+    @connections.get(sig).get_peername
   end
 
   def get_sock_name(sig)
+    @connections.get(sig).get_sockname
   end
 
   def attach_channel(sc, watch_mode)
+    
   end
 
   def detach_channel(sig)
@@ -294,9 +411,11 @@ class EmReactor
   end
 
   def is_notify_writable(sig)
+    @connections.get(sig).is_notify_writable(true)
   end
 
   def connection_count
+    @connections.count() + @acceptors.size()
   end
   
 end
